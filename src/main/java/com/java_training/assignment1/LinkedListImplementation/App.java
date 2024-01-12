26c4bae18c8fd6dfe4f0c9ad203b3bea4dd35857
Hitesh Chavhan <chavhanhitesh0811@gmail.com>
2024-01-12 15:12:29 +0530

    initial commit

--- src/main/java/com/java_training/assignment1/LinkedListImplementation/App.java
+++ src/main/java/com/java_training/assignment1/LinkedListImplementation/App.java
@@ -11,7 +11,6 @@ public class App {
 	
 	private static Logger logger = LogManager.getLogger(App.class);
 
-//	main function
 	public static void main(String[] args) {
 		
 		TaskImplementation taskImpl = new TaskImplementation();
